1922e47467864d67a9fadffa95e0cec69a39c9ed
Sarthak-Aswal <sarthakaswalgit@gmail.com>
2025-03-03 19:50:28 +0530

    add footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import Navbar from '@/components/navbar';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         >
           <Navbar />
           <div className="mt-16">{children}</div>
+          <Footer/>
         </ThemeProvider>
       </body>
     </html>
