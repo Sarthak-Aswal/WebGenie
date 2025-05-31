f00f6d9e929377ee3a0dd6c33c89e3b1f671d755
Sarthak-Aswal <sarthakaswalgit@gmail.com>
2025-06-01 00:38:19 +0530

    toast

--- components/ClientLottie.tsx
+++ components/ClientLottie.tsx
@@ -1,21 +1,15 @@
 // components/ClientLottie.tsx
 'use client';
-import Script from 'next/script';
-import { Player } from "@lottiefiles/react-lottie-player";// or 'react-lottie-player'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'// or 'react-lottie-player'
 
 
 export default function ClientLottie() {
   return (
-    <>
-    <Script
-        src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"
-        strategy="beforeInteractive"
-      />
-   <Player
-      src="https://lottie.host/c98831ce-1f8b-406a-9d86-0bbad7aff19b/fH6AvOKMzh.lottie"  // place the .lottie file in public/animations
-      autoplay
+    <DotLottieReact
+      src="https://lottie.host/c98831ce-1f8b-406a-9d86-0bbad7aff19b/fH6AvOKMzh.lottie"
       loop
-      style={{ height: "300px", width: "300px" }}
-    /></>
+      autoplay
+    />
   );
-}
+  
+};
